ae9969050d98f3b1e5b51472995d57d2f18a2366
lambospeed <lambospeed129@gmail.com>
2018-03-23 02:58:05 +0800

    loading gif

--- application/views/awards/vote.php
+++ application/views/awards/vote.php
@@ -1,10 +1,10 @@
 <!-- About start here -->
 <section class="about about-six padding-120">
     <div class="container">
+        <div class="loader"></div>
         <div class="section-header text-center">
             <h2>Place Your Votes!</h2>
         </div>
-        <div class="loader"></div>
         <div id="surveyContainer"></div>
         <ul class="about-button">
             <li><a href="<?= base_url(); ?>vote/view" class="default-button">View Nominees</a></li>
